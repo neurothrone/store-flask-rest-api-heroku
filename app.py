2fca29e3b425e2d428e24b2b4207c1b0e7c52d05
Zaid Neurothrone <zaid.neurothrone@protonmail.com>
2021-07-19 15:22:50 +0200

    modify postgres url from environ var

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ from resources.store import StoreResource, StoreListResource
 from resources.user import UserRegisterResource
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL",
+postgresql_url = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(postgresql_url,
                                                        "sqlite:///data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = os.urandom(24)
