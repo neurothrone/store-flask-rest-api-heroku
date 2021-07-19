eeab07b609816ee12579207cb97374b1cfee3b99
Zaid Neurothrone <zaid.neurothrone@protonmail.com>
2021-07-19 02:33:53 +0200

    attempt to fix python app not found

--- app.py
+++ app.py
@@ -11,21 +11,20 @@ from resources.item import ItemResource, ItemListResource
 from resources.store import StoreResource, StoreListResource
 from resources.user import UserRegisterResource
 
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = os.urandom(24)
+api = Api(app)
+jwt = JWT(app, authenticate, identity)  # creates new endpoint: /auth
+
+api.add_resource(ItemResource, "/item/<string:name>")
+api.add_resource(ItemListResource, "/items")
+api.add_resource(StoreResource, "/store/<string:name>")
+api.add_resource(StoreListResource, "/stores")
+api.add_resource(UserRegisterResource, "/register")
 
 if __name__ == "__main__":
-    app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///data.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.secret_key = os.urandom(24)
-    api = Api(app)
-    jwt = JWT(app, authenticate, identity)  # creates new endpoint: /auth
-
-    api.add_resource(ItemResource, "/item/<string:name>")
-    api.add_resource(ItemListResource, "/items")
-    api.add_resource(StoreResource, "/store/<string:name>")
-    api.add_resource(StoreListResource, "/stores")
-    api.add_resource(UserRegisterResource, "/register")
-
     # db.init_app(app)
     #
     # @app.before_first_request
